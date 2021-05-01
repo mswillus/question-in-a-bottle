00f4bcbe71ed3e1c1feb5db69bfcdc0a7edfb0e8
Mark <m.swillus@tudelft.nl>
2021-05-01 17:35:41 +0200

    refactor

--- app.py
+++ app.py
@@ -5,27 +5,31 @@ from os import environ
 
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
 
-if "DATA_DIR" in environ:
-    app.config["DATA_DIR"] = environ["DATA_DIR"]
-else:
-    app.config["DATA_DIR"] = tempfile.mkdtemp()
+def create_app():
+    # create and configure the app
+    app = Flask(__name__)
 
-app.logger.setLevel(logging.INFO)
+    if "DATA_DIR" in environ:
+        app.config["DATA_DIR"] = environ["DATA_DIR"]
+    else:
+        app.config["DATA_DIR"] = tempfile.mkdtemp()
 
+    app.logger.setLevel(logging.INFO)
 
-@app.route("/<survey>", methods=["POST"])
-def results(survey):
-    """Handles results form surveys"""
-    result_file = tempfile.mkstemp(dir=app.config["DATA_DIR"])
-    try:
-        json_response = json.loads(request.data)
-        result_file.write(json.dumps(json_response, indent=2, sort_keys=True))
-    except json.JSONDecodeError as e:
-        app.logger.error(e.msg)
-    return jsonify({})
+    @app.route("/<survey>", methods=["POST"])
+    def results(survey):
+        """Handles results form surveys"""
+        result_file = tempfile.mkstemp(dir=app.config["DATA_DIR"])
+        try:
+            json_response = json.loads(request.data)
+            result_file.write(json.dumps(json_response, indent=2, sort_keys=True))
+        except json.JSONDecodeError as e:
+            app.logger.error(e.msg)
+        return jsonify({})
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run()
+    create_app().run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,12 @@ import pytest
 from flask import current_app
 from pytest_httpserver import HTTPServer
 
-from ..app import app
+from ..app import create_app
 
 
 @pytest.fixture(scope="function")
 def client(httpserver: HTTPServer):
+    app = create_app()
     app.config["PUBLIC_URL"] = httpserver.url_for("")
     app.config["TESTING"] = True
 
@@ -27,5 +28,13 @@ def test_calling_restults_with_post_method_returns_200(client):
 
 
 def test_default_path_is_in_temp_dir():
+    app = create_app()
     with app.app_context():
         assert app.config["DATA_DIR"].startswith("/tmp")
+
+
+def test_path_override():
+    os.environ["DATA_DIR"] = "./"
+    app = create_app()
+    with app.app_context():
+        assert app.config["DATA_DIR"].startswith("./")
